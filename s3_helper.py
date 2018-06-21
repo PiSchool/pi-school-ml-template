a9cdd315a9b02a3f7d2ded0e5715d5771c0566e0
Simone Scardapane <simone.scardapane@uniroma1.it>
2018-06-21 10:34:03 +0200

    Moved configuration to JSON.

--- s3_helper.py
+++ s3_helper.py
@@ -6,19 +6,23 @@ import boto3
 from sklearn.externals import joblib
 import subprocess
 
-import project_configuration
+import json
 import logger
 log = logger.getLogger()
 
 
 def get_data():
     """ Get your data from s3 if they does not already  exist localy"""
-    config = project_configuration.get_config()
+    
+    # Load configuration
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
+    
     # check if data already exist
     last_hash = ""
     current_hash = str(hash_data())
-    if os.path.isfile(config["dataHashFile"]):
-        file_reader = open(config["dataHashFile"])
+    if os.path.isfile(data['local']["dataHashFile"]):
+        file_reader = open(data['local']["dataHashFile"])
         last_hash = file_reader.read()
         file_reader.close()
 
@@ -31,12 +35,12 @@ def get_data():
         if os.name == 'nt':
             # windows use aws_cli method
             aws_cli(['s3', 'sync',
-                     f's3://{config["bucketName"]}/{config["bucketDataPath"]}',
-                     config['bucketDataPath']])
+                     f's3://{data["s3"]["bucketName"]}/{data["s3"]["bucketDataPath"]}',
+                     data["local"]['localDataPath']])
         else:
             # unix use simply aws s3 sync
             s3SyncOutput = subprocess.check_output(
-                ["aws", "s3", "sync", f's3://{config["bucketName"]}/{config["bucketDataPath"]}', config["localDataPath"]]).strip().decode("utf-8")
+                ["aws", "s3", "sync", f's3://{data["s3"]["bucketName"]}/{data["s3"]["bucketDataPath"]}', data["local"]["localDataPath"]]).strip().decode("utf-8")
             log.info(["s3SyncOutput:", s3SyncOutput])
         # session = boto3.Session()
         # s3 = session.resource('s3')
@@ -46,24 +50,26 @@ def get_data():
         #     f'{config["bucketDataPath"]}data-set.csv', f'{config["localDataPath"]}data-set.csv')
         # when we download data, we save in a file a hash representing the distant data directory
         # like this we do not download several time unchanged data.
-        log.info(f'{current_hash} will be added to {config["dataHashFile"]}')
-        file_writer = open(config["dataHashFile"], "w")
+        log.info(f'{current_hash} will be added to {data["local"]["dataHashFile"]}')
+        file_writer = open(data["local"]["dataHashFile"], "w")
         file_writer.write(current_hash)
         file_writer.close()
     else:
         log.info("data already existing localy, no need to download it again")
 
-    return f'{config["localDataPath"]}data-set.csv'
+    return f'{data["local"]["localDataPath"]}data-set.csv'
 
 
 def hash_data():
     """ list data directory of the project on s3 and get a hash representing it. 
     This allow to detect changes in the dataset. """
-    config = project_configuration.get_config()
+        # Load configuration
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
     session = boto3.Session()
     s3 = session.resource('s3')
     hasher = hashlib.md5()
-    [hasher.update(x.key.encode('utf-8')) for x in s3.Bucket(config["bucketName"]).objects.filter(
+    [hasher.update(x.key.encode('utf-8')) for x in s3.Bucket(data["s3"]["bucketName"]).objects.filter(
         Prefix='data/')]
     data_h = hasher.hexdigest()
     log.info(f"hash : {data_h} ")
@@ -72,18 +78,22 @@ def hash_data():
 
 def get_model(model_name, ext='pkl'):
     """ Get the model you previously saved from s3 """
-    config = project_configuration.get_config()
+    # Load configuration
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
     session = boto3.Session()
     s3 = session.resource('s3')
-    s3.Bucket(config["bucketName"]).download_file(
-        f'{config["modelPath"]}{model_name}.{ext}', f'{config["modelLocalPath"]}{model_name}.{ext}')
-    return f'{config["modelLocalPath"]}{model_name}.{ext}'
+    s3.Bucket(data["s3"]["bucketName"]).download_file(
+        f'{data["s3"]["modelPath"]}{model_name}.{ext}', f'{data["local"]["modelPath"]}{model_name}.{ext}')
+    return f'{data["local"]["modelPath"]}{model_name}.{ext}'
 
 
 def save_and_push_model(model, ext='pkl'):
     """ save you model in a file and send it to s3 """
-    config = project_configuration.get_config()
-    local_file = f'{config["modelLocalPath"]}{model.name}.{ext}'
+    # Load configuration
+    with open('config.json') as json_data_file:
+        data = json.load(json_data_file)
+    local_file = f'{data["local"]["modelLocalPath"]}{model.name}.{ext}'
     # dump your sklearn model into a file
     joblib.dump(model, local_file)
 
@@ -91,8 +101,8 @@ def save_and_push_model(model, ext='pkl'):
     s3 = session.resource('s3')
     # upload model to s3
     data = open(local_file, 'rb')
-    s3.Bucket(config["bucketName"]).put_object(
-        Key=f'{config["modelPath"]}{model.name}.{ext}', Body=data)
+    s3.Bucket(data["s3"]["bucketName"]).put_object(
+        Key=f'{data["s3"]["modelPath"]}{model.name}.{ext}', Body=data)
 
 
 def aws_cli(*cmd):
